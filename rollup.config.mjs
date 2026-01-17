20ae90c249af899a65f6e69d0bfd7bbe7ddd0b90
josvink66 <jos@q-mod.nl>
2026-01-17 14:37:18 +0100

    Updated to version: v0.0.38

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
   input: 'grocy-chores-card.js',
   output: {
-    dir: 'output',
+    dir: 'output',       // build-output blijft in output/
     format: 'es'
   },
   plugins: [nodeResolve()]
